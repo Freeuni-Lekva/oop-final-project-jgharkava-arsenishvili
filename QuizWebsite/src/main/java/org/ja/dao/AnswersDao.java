8ee9e4301388ba33abdd3676588003f36b85c650
njvar23 <njvar23@freeuni.edu.ge>
2025-07-08 17:36:17 +0400

    Update AnswersDao.java

--- QuizWebsite/src/main/java/org/ja/dao/AnswersDao.java
+++ QuizWebsite/src/main/java/org/ja/dao/AnswersDao.java
@@ -117,8 +117,8 @@ public class AnswersDao {
         if(answer==null){
             return false;
         }
-        String sql = "SELECT COUNT(*) FROM answers WHERE answer_id = ? AND question_id=?" +
-                "AND answer_text=? AND answer_order = ?" +
+        String sql = "SELECT COUNT(*) FROM answers WHERE answer_id = ? AND question_id=? " +
+                "AND answer_text=? AND answer_order = ? " +
                 " AND answer_validity = ?";
 
         try (Connection connection = dataSource.getConnection();
