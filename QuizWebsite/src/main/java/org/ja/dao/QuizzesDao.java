0570879d880b2d57317c843b5d94ab8cac1cabce
njvar23 <njvar23@freeuni.edu.ge>
2025-07-08 18:02:34 +0400

    Update QuizzesDao.java

--- QuizWebsite/src/main/java/org/ja/dao/QuizzesDao.java
+++ QuizWebsite/src/main/java/org/ja/dao/QuizzesDao.java
@@ -382,14 +382,12 @@ public class QuizzesDao {
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
             ps.setLong(1, id); // Properly set the parameter
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                return retrieveQuiz(rs); // Move to the first row and extract data
-            } else {
-                return null; // or throw an exception if appropriate
-            }
-
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return retrieveQuiz(rs); // Move to the first row and extract data
+                } else {
+                    return null; // or throw an exception if appropriate
+                }
         } catch (SQLException e) {
             throw new RuntimeException("Error retrieving quiz by ID", e);
         }
