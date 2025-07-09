50d484d45e601f59f4e017a38f4c1e6bd3ceed48
ChorniBero <toberi23@freeuni.edu.ge>
2025-07-09 16:05:26 +0400

    added review at quiz result page

--- QuizWebsite/src/main/java/org/ja/servlet/ChallengeServlet.java
+++ QuizWebsite/src/main/java/org/ja/servlet/ChallengeServlet.java
@@ -11,18 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/challenge-servlet")
+@WebServlet("/challenge-friend")
 public class ChallengeServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) {
         ChallengesDao challengesDao = (ChallengesDao) getServletContext().getAttribute(Constants.ContextAttributes.CHALLENGES_DAO);
 
-        HttpSession session = req.getSession();
-
-        User sender = (User) session.getAttribute(Constants.SessionAttributes.USER);
+        User sender = (User) req.getSession().getAttribute(Constants.SessionAttributes.USER);
         long receiverId = Long.parseLong(req.getParameter("challenged-id"));
         long quizId = Long.parseLong(req.getParameter(Constants.RequestParameters.QUIZ_ID));
 
+        // TODO will be boolean in the near future
         challengesDao.insertChallenge(new Challenge(sender.getId(), receiverId, quizId));
     }
 }
